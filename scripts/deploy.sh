970840ec908f3757254b0ac1f6374d81025869e2
Oluwafemi Olofintuyi <oluwafemi.olofintuyi@gmail.com>
2019-12-07 14:25:19 -0600

    update nginx config and deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,12 +7,14 @@ COMMIT="$TRAVIS_COMMIT"
 BRANCH="$TRAVIS_BRANCH"
 IMAGE_REPO="506127536868.dkr.ecr.eu-west-1.amazonaws.com"
 APP="typescript-node-starter"
+CONTAINER_NAME="starter"
 
 case "$BRANCH" in
     feature/work-sample)
     export TAG="$BRANCH"
     export ENV="dev"
     export CLUSTER_NAME="typescript-node-starter"
+    export domain_name="sandbox.eha.im"
     ;;
 esac
 
@@ -30,9 +32,10 @@ if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
   docker push "$IMAGE_REPO/$APP-$ENV:$COMMIT"
 
   echo build and push nginx image
+  envsubst '${domain_name}' < nginx/nginx.tpl.conf > nginx/nginx.conf
   docker build -t "$IMAGE_REPO/$APP-nginx-$ENV:latest" nginx/.
   docker push "$IMAGE_REPO/$APP-nginx-$ENV:latest"
 
   echo deploy a new version of the service
-  ecs deploy --timeout 600 ${CLUSTER_NAME}-${ENV} ${APP}
+  ecs deploy --timeout 900 $CLUSTER_NAME-$ENV $APP -i $CONTAINER_NAME "$IMAGE_REPO/$APP-$ENV:$COMMIT"
 fi
